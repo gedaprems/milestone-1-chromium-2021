09cd533d18f4a0df6883ade6f559f2c02b059909
Premsagar <gedamprems2@gmail.com>
2021-03-26 01:44:41 +0530

    Successfully Created and Edit all files

--- serviceworker.js
+++ serviceworker.js
@@ -1,9 +1,9 @@
-var staticCacheName = "pwa";
+var staticCacheName = "ms1";
 
 self.addEventListener("install", function (e) {
 e.waitUntil(
 	caches.open(staticCacheName).then(function (cache) {
-	return cache.addAll(["/"]);
+	return cache.addAll(["./","./index.html","./page2.html","./page3.html"]);
 	})
 );
 });
